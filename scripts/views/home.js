b700f1059e510b395ddbd4006a9517429c2989bb
onehunnid <climbrick1989@gmail.com>
2015-10-01 10:15:57 -0400

    initial commit

---
+++ scripts/views/home.js
@@ -0,0 +1,18 @@
+var Home = (function() {
+
+	// DOM Handlers
+
+	// KICKSTART VIEW
+	function initHome() {
+
+		// load main content
+		$('#main-content').html("Welcome to the Single Page Application Kit!");
+
+		// bind events
+
+	}
+	return {
+		init: initHome
+	};
+
+})();
